4aa2213a529fd3a6b5ba28d4b764d4ba864bde24
ayekaday <yukiakd279@gmail.com>
2025-01-04 22:52:42 +0630

    add delete button

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1>Vue JS</h1>
+  <div v-if="showModal">
+    <ModalContent :theme="theme" @close="showModal = false">
+      <h1>Login Success</h1>
+      <p>Welcome Home</p>
+    </ModalContent>
+  </div>
+  <button @click="showModal = true">open modal</button>
+
+  <div v-if="deleteModal">
+    <ModalContent :theme="theme" @close="deleteModal = false">
+      <h1>Are you delete?</h1>
+      <p>Sure?</p>
+    </ModalContent>
+  </div>
+  <button @click="deleteModal = true">delete</button>
+</template>
+
+<script>
+import ModalContent from "./components/ModalContent.vue";
+
+export default {
+  components: {
+    ModalContent,
+  },
+  data() {
+    return {
+      header: "Login Success",
+      content: "Welcome to my page",
+      theme: "delete",
+      showModal: false,
+      deleteModal: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: wheat;
+}
+</style>
---
+++ src/components/ModalContent.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div
+      class="modal"
+      :class="{
+        success: theme === 'success',
+        danger: theme === 'delete',
+      }"
+    >
+      <slot>default</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.backdrop {
+  background-color: rgba(0, 0, 0, 0.2);
+  width: 100%;
+  height: 100%;
+  top: 0;
+  position: fixed;
+}
+.modal {
+  color: rgb(226, 43, 43);
+  background-color: rgb(13, 164, 205);
+  width: 400px;
+  margin: 100px auto;
+  padding: 20px;
+  border-radius: 10px;
+}
+p {
+  font-style: normal;
+}
+.success {
+  background-color: green;
+  color: blanchedalmond;
+}
+.danger {
+  background-color: red;
+  color: bisque;
+}
+</style>
